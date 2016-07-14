ac59457fca91cab74dd0dcec75a994bbb23070ee
amo102 <amo102@student.uib.no>
2016-07-14 14:12:49 +0200

    Formatting

--- android/src/org/ajm/laforkids/AndroidLauncher.java
+++ android/src/org/ajm/laforkids/AndroidLauncher.java
@@ -1,15 +1,14 @@
 package org.ajm.laforkids;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Adapter(), config);
-	}
+public class AndroidLauncher extends AndroidApplication   {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+                initialize(new Adapter(), config);
+        }
 }
