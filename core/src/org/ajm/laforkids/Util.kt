076387c9cad6826577a7f9746a9d942daf8591c0
amo102 <amo102@student.uib.no>
2016-07-17 00:31:58 +0200

    New method

--- core/src/org/ajm/laforkids/Util.kt
+++ core/src/org/ajm/laforkids/Util.kt
@@ -1,5 +1,7 @@
 package org.ajm.laforkids
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.ui.Table
@@ -19,4 +21,11 @@ fun getAllChildren(table: Group): Array<Actor> {
     }
 
     return getAllChildren(table, Array<Actor>())
+}
+
+fun getTextWidth(font: BitmapFont, text: String): Float {
+    val layout = GlyphLayout()
+    layout.setText(font, text)
+    val width = layout.width
+    return width
 }
