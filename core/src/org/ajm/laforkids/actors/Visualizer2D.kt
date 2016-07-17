ce317a4a5c8c238cec041484edb7a2ee21af5985
amo102 <amo102@student.uib.no>
2016-07-17 14:36:23 +0200

    Names can be shown at the points

--- core/src/org/ajm/laforkids/actors/Visualizer2D.kt
+++ core/src/org/ajm/laforkids/actors/Visualizer2D.kt
@@ -3,6 +3,8 @@ package org.ajm.laforkids.actors
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Actor
@@ -18,9 +20,13 @@ class Visualizer2D : Actor {
 
     private var beforeX = 0
     private var beforeY = 0
+    private var beforeName = "B"
 
     private var afterX = 0
     private var afterY = 0
+    private var afterName = "C"
+    private var afterXStringWidth = 0f
+    private var afterYStringWidth = 0f
 
     private var drawAfterX = false
     private var drawAfterY = false
@@ -33,9 +39,12 @@ class Visualizer2D : Actor {
     /** Used for drawing stuff */
     private val dot: TextureRegion
     private val multiplicationTable: MultiplicationTable
+    private val font: BitmapFont
 
     constructor(skin: Skin, multiplicationTable: MultiplicationTable) {
         touchable = Touchable.disabled
+
+        font = skin.getFont("default")
 
         val left = multiplicationTable.matrixLeft
         val leftEntries = left.matrixColumns * left.matrixRows
@@ -69,27 +78,40 @@ class Visualizer2D : Actor {
             if (left.matrixRows == 1 && left.matrixColumns == 2) {
                 beforeX = left.get(0, 0).toInt()
                 beforeY = left.get(0, 1).toInt()
+                beforeName = left.backgroundText
             } else if (left.matrixRows == 2 && left.matrixColumns == 1) {
                 beforeX = left.get(0, 0).toInt()
                 beforeY = left.get(1, 0).toInt()
+                beforeName = left.backgroundText
             } else if (right.matrixRows == 1 && right.matrixColumns == 2) {
                 beforeX = right.get(0, 0).toInt()
                 beforeY = right.get(0, 1).toInt()
+                beforeName = right.backgroundText
             } else if (right.matrixRows == 2 && right.matrixColumns == 1) {
                 beforeX = right.get(0, 0).toInt()
                 beforeY = right.get(1, 0).toInt()
+                beforeName = right.backgroundText
             } else throw IllegalStateException()
 
             if (product.matrixRows == 1 && product.matrixColumns == 2) {
                 afterX = computeProductEntry(0, 0)
                 afterY = computeProductEntry(0, 1)
+                afterName = product.backgroundText
             } else if (product.matrixRows == 2 && product.matrixColumns == 1) {
                 afterX = computeProductEntry(0, 0)
                 afterY = computeProductEntry(1, 0)
+                afterName = product.backgroundText
             } else throw IllegalStateException()
         } catch (e: NumberFormatException) {
             // may happen text is just "-" (minus)
         }
+
+        val layout = GlyphLayout()
+        layout.setText(font,afterX.toString())
+        afterXStringWidth = layout.width
+        layout.setText(font,afterY.toString())
+        afterYStringWidth = layout.width
+
 
         drawAfterX = !product.get(0).isEmpty()
         drawAfterY = !product.get(1).isEmpty()
@@ -113,6 +135,10 @@ class Visualizer2D : Actor {
 
         val lineThickness = (2f + 6f * Math.min(width, height) / (1000f)).toInt().toFloat()
         val pos = localToStageCoordinates(Vector2())
+        val finalPos = Vector2()
+
+        val beforeScale = font.data.scaleX
+        font.data.setScale(0.75f)
 
         // draw axes
         batch.color = axisColor
@@ -128,23 +154,39 @@ class Visualizer2D : Actor {
         if (afterY != 0) scale = Math.min(Math.abs(dim / afterY), scale)
         scale *= 0.9f
 
-        // draw crosses representing the matrices(vectors) with 2 entries
-        batch.color = beforeColor
-        cross(batch, pos.x + width / 2f + beforeX * scale, pos.y + height / 2f + beforeY * scale, lineThickness)
 
+        // mark the axes
         if (drawAfterX) {
             batch.color = axisColor
-            cross(batch, pos.x + width / 2f + afterX * scale, pos.y + height / 2f, lineThickness)
+            font.color = axisColor
+            finalPos.set(pos.x + width / 2f + afterX * scale, pos.y + height / 2f)
+            cross(batch, finalPos.x, finalPos.y, lineThickness)
+            font.draw(batch, afterX.toString(), finalPos.x-afterXStringWidth/2f, finalPos.y-font.capHeight/2)
         }
         if (drawAfterY) {
             batch.color = axisColor
-            cross(batch, pos.x + width / 2f, pos.y + height / 2f + afterY * scale, lineThickness)
+            font.color = axisColor
+            finalPos.set(pos.x + width / 2f, pos.y + height / 2f + afterY * scale)
+            cross(batch, finalPos.x, finalPos.y, lineThickness)
+            font.draw(batch, afterY.toString(), finalPos.x+font.spaceWidth, finalPos.y+font.capHeight/2)
         }
+
+        // draw crosses representing the matrices(vectors) with 2 entries
+        batch.color = beforeColor
+        font.color = beforeColor
+        finalPos.set(pos.x + width / 2f + beforeX * scale, pos.y + height / 2f + beforeY * scale)
+        cross(batch, finalPos.x, finalPos.y, lineThickness)
+        font.draw(batch, beforeName, finalPos.x+font.spaceWidth, finalPos.y)
 
         if (drawAfterX && drawAfterY) {
             batch.color = afterColor
-            cross(batch, pos.x + width / 2f + afterX * scale, pos.y + height / 2f + afterY * scale, lineThickness)
+            font.color = afterColor
+            finalPos.set(pos.x + width / 2f + afterX * scale, pos.y + height / 2f + afterY * scale)
+            cross(batch, finalPos.x, finalPos.y, lineThickness)
+            font.draw(batch, afterName, finalPos.x+font.spaceWidth, finalPos.y)
         }
+
+        font.data.setScale(beforeScale)
     }
 
     private fun cross(batch: Batch, x: Float, y: Float, thickness: Float) {
